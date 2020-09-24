8d61327658662995d3203a35b3d96c8328e1d4d8
harisri9948 <harisri9948@gmail.com>
2020-09-24 15:53:56 +0530

    fixed the review comments

--- applications/bookmyshow/lib/home_page.py
+++ applications/bookmyshow/lib/home_page.py
@@ -7,12 +7,11 @@ import time
 
 
 
-class  HomePage():
+class  HomePage:
     def __init__(self):
         self.driver_path = "C:\\Users\\srihari\\PycharmProjects\\AutomationFramework\\dependencies\\drivers\\chrome\\chromedriver.exe"
-        L = Logger()
         if not os.path.exists(self.driver_path):
-            L.log_error(f"Not able to find web driver. path {self.driver_path}")
+            Logger.log_error(f"Not able to find web driver. path {self.driver_path}")
         self.driver = None
         self.launch_app()
         self.search_city()
@@ -25,57 +24,50 @@ class  HomePage():
         #self.navigate_movies()
 
     def launch_app(self, timeout=10):
-        L = Logger()
         try:
 
             self.driver = webdriver.Chrome(executable_path=self.driver_path)
             self.driver.get("https://in.bookmyshow.com/")
             time.sleep(timeout)
-            L.log_info("launching application is successfull")
+            Logger.log_info("launching application is successfull")
         except Exception as e:
-            L.log_error("Error while launching the application")
+            Logger.log_error("Error while launching the application")
 
 
     def search_city(self):
-        L = Logger()
         try:
 
             self.searchcity= self.driver.find_element(By.XPATH,Locator.Searchcity)
             self.searchcity.send_keys("pune")
-            L.log_info("searching succesfully")
+            Logger.log_info("searching succesfully")
 
         except Exception as e:
-            L.log_error("error while searching city")
+            Logger.log_error("error while searching city")
     def select_city(self):
-
-        L = Logger()
         try:
             time.sleep(2)
             self.selectcity = self.driver.find_element(By.XPATH,Locator.Selectcity)
             self.selectcity.click()
-            L.log_info("city selected")
+            Logger.log_info("city selected")
         except Exception as e:
-            L.log_error("error while clicking city")
+            Logger.log_error("error while clicking city")
 
     def launch_app_with_popup_suppressed(self):
-        L = Logger()
         try:
-
             time.sleep(10)
             self.pop_up = self.driver.find_element(By.XPATH,Locator.pop_up)
             self.pop_up.click()
-            L.log_info("pop_up handled succesfully")
+            Logger.log_info("pop_up handled succesfully")
         except Exception as e:
-            L.log_error("Error while handling pop_up")
+            Logger.log_error("Error while handling pop_up")
 
     def navigate_movies(self):
-        L = Logger()
         try:
             self.Movies = self.driver.find_element(By.XPATH,Locator.Movies)
             self.Movies.click()
-            L.log_info("movies open succesfully")
+            Logger.log_info("movies open succesfully")
         except Exception as e:
-            L.log_error("error occurred while navigating to movies")
+            Logger.log_error("error occurred while navigating to movies")
 
 
 if __name__ == "__main__":
--- applications/bookmyshow/lib/loggers.py
+++ applications/bookmyshow/lib/loggers.py
@@ -7,16 +7,19 @@ logging.basicConfig(format='%(asctime)s::%(filename)s::%(lineno)s::%(funcName)s\
                     ::%(msg)s', level =logging.INFO , filename='home_info.log', filemode='a')
 
 logger = logging.getLogger(__name__)
-class Logger():
-
+class Logger:
+    """
     def __init__(self,logger =None):
         self.logger = logger or logging.getLogger(__name__)
-
+    """
+    @classmethod
     def log_info(self, msg):
-        self.logger.info(msg)
-    def log_error(self, msg):
-        self.logger.error(msg)
-    def log_warning(self, msg):
-        self.logger.warning(msg)
+        logger.info(msg)
 
-L = Logger()
+    @classmethod
+    def log_error(self, msg):
+        logger.error(msg)
+
+    @classmethod
+    def log_warning(self, msg):
+        logger.warning(msg)
